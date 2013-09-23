9f7084cc653b77c553f06b1a425bc36f3a08497c
Emily Dirsh <edirsh@redhat.com>
2013-09-22 23:41:19 -0400

    Add basic server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app =  express();
+
+// Serve static client-side files
+app.use(
+	express.static(__dirname + '../client/www')
+);
+
+app.listen(3000);
+console.log('Listening on port 3000');

7de152f2ed5fd59e5614e5b12aef5bfc04f0abb3
Emily Dirsh <edirsh@redhat.com>
2013-09-22 23:47:23 -0400

    Fix small bug in server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app =  express();
 
 // Serve static client-side files
-app.use(
-	express.static(__dirname + '../client/www')
-);
+app.use(express.static(__dirname + '/../client/www'));
 
 app.listen(3000);
 console.log('Listening on port 3000');

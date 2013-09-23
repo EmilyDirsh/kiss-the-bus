0b18603cc0b6fb8ed51798cb916dc4dd0d456a5d
Emily Dirsh <edirsh@redhat.com>
2013-09-22 23:54:21 -0400

    Add hello test route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app =  express();
 
+//Hello test route
+app.get('/hello', function (req, res) {
+	res.send('hello');
+}
+
 // Serve static client-side files
 app.use(express.static(__dirname + '/../client/www'));
 

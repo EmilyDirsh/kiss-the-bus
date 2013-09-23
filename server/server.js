f350a9cc6ffdc029a4cc083efb1140b0c3c4dbdc
liemily <liemily@toasty.bread>
2013-09-23 00:00:26 -0400

    NOW the syntax is fixed with both semicolon and parans

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var app =  express();
 //Hello test route
 app.get('/hello', function (req, res) {
 	res.send('hello');
-};
+});	
 
 // Serve static client-side files
 app.use(express.static(__dirname + '/../client/www'));

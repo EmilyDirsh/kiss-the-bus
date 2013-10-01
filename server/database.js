463641d6439d491f1c73579565d43afceabcdd26
liemily <liemily@toasty.bread>
2013-09-30 20:30:33 -0400

    Created first pass schema

---
+++ server/database.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// types: routes, stops, trips
+// properties for each type:
+//   route: mode_name, route_id, route_name, direction_id, direction_name, stop_sequence
+//      stop_id, stop_name, sch_arr_dt, sch_dep_dt
+//   stops: stop_id, stop_name, stop_lat, stop_lon
+//   dateTime: sch_arr_dt, sch_dep_dt, stop_id, route_id
+//   
+
+
+
+var route = new Schema({
+  stopsForRoute:   [{stopId:   String, stopName:   String, directionName:   String}],
+  routeId:   String,
+  routeName:   String,
+  modeName:   String,
+//  endPoint:
+});
+
+var stop = new Schema({
+  modeName:   String,
+  stopId:   String,
+  stopName:   String,
+  stopLat:   Number,
+  stopLon:   Number,
+  schedule:   [{routeName:   String, directionName:   String, times:   [Date]}]
+});

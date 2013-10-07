941ff0d275c93bed71f764d2e9668455c2fa80f7
liemily <liemily@toasty.bread>
2013-10-06 23:55:29 -0400

    added db connection logic

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app =  express();
-	
+var mongoose = require('mongoose');
+
+/* Connect to database */
+mongoose.connect('mongodb://localhost/kissthebus');
+var db = mongoose.connection();
+db.on('error', console.error.bind(console, 'connection error;'));
+db.once('open', function() {
+	console.log('connection created');
+});
 
 // Serve static client-side files
 app.use(express.static(__dirname + '/../client/www'));
